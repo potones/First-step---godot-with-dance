24242c6a1fd30207af761cbf790ca062994b5fd6
Yeoubi Company <41808277+potones@users.noreply.github.com>
2026-06-22 00:56:42 +0900

    오늘은 에셋관련 고민을 했다
    하지만 소스는 관련이 없다

---
+++ first-step---godot-with-dance/Scripts/good_dino.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print('sax Knight!')
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_canvas_layer_visibility_changed() -> void:
+	pass # Replace with function body.
